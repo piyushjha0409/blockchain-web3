a1eac626723219cf8ac445c5f2cd4f5b956e130c
piyush <peeyush0409@gmail.com>
2023-03-13 19:33:12 +0530

    initialized whitelist project

--- Tutorial/contracts/constructor.sol
+++ Tutorial/contracts/constructor.sol
@@ -3,10 +3,13 @@ pragma solidity ^0.8.10;
 
 contract X {
  
- string public name;
+string public name;
 
  //You will need to provide a string argument when deploying to the contract 
- constructor(string memory _name){
+
+constructor(string memory _name){
     name = _name; //assigning the value to the name
- }
+}
+
+
 }
