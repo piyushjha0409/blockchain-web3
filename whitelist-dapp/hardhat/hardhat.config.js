df4e8fb7749c7234eaed5d5ec87f5594977fe5b9
piyushjha0409 <peeyush0409@gmail.com>
2023-03-14 00:04:43 +0530

    deployed the contract now frontend to do nextjs

--- whitelist-dapp/hardhat/hardhat.config.js
+++ whitelist-dapp/hardhat/hardhat.config.js
@@ -1,6 +1,17 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config({ path: ".env" });
+
+
+const INFURA_HTTP_URL = process.env.INFURA_HTTP_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.18",
+  networks:{
+    goerli:{
+      url: INFURA_HTTP_URL,
+      accounts:[PRIVATE_KEY]
+    },
+  },
 };
