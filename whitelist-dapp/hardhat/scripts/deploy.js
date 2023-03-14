04205c5b2a902db11aaa8052d729f6a53c93b810
piyushjha0409 <peeyush0409@gmail.com>
2023-03-14 19:25:10 +0530

    created a next app

--- whitelist-dapp/hardhat/scripts/deploy.js
+++ whitelist-dapp/hardhat/scripts/deploy.js
@@ -2,16 +2,16 @@ const {ethers} = require("hardhat");
 
 async function main (){
  
-    const whiteListContract = await ethers.getContractFactory("whiteList")
+const whiteListContract = await ethers.getContractFactory("whiteList")
 
-    //here we deploy the contract 
-    const deployedWhitelistContract = await whiteListContract.deploy(20); //20 is the max contracts that can be added
-    
-    //wait for it to finish  deploying 
-    await deployedWhitelistContract.deployed();
+//here we deploy the contract 
+const deployedWhitelistContract = await whiteListContract.deploy(20); //20 is the max contracts that can be added
 
-    //print the address of the contract after deploying the contract
-    console.log("Deployed contract address: ", deployedWhitelistContract.address);
+//wait for it to finish  deploying 
+await deployedWhitelistContract.deployed();
+
+//print the address of the contract after deploying the contract
+console.log("Deployed contract address: ", deployedWhitelistContract.address);
 }
 
 main().then(() =>process.exit(0)).catch((err)=>{
