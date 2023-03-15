65a2a0eb5c1d3a570b7b638dbd01160851acdebf
piyushjha0409 <peeyush0409@gmail.com>
2023-03-15 16:41:31 +0530

    updating the index.js

--- whitelist-dapp/myapp/src/pages/index.js
+++ whitelist-dapp/myapp/src/pages/index.js
@@ -4,7 +4,7 @@ import { providers, Contract } from 'ethers'
 import { useState, useRef, useEffect } from 'react'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-import { WHITELIST_CONTRACT_ADDRESS, abi } from "../constants";
+// import { WHITELIST_CONTRACT_ADDRESS, abi } from "../constants";
 
 
 export default function Home() {
@@ -18,10 +18,39 @@ export default function Home() {
   const [loading, setLoading] = useState(false);
 
   //numberOfWhiteListed tracks the number addresses whitelisted
-  const [numberOfWhiteListed, setNumberOfWhiteListed] = useState(0)
+  const [numberOfWhiteListed, setNumberOfWhiteListed] = useState(0);
+
+  //
+
   return (
     <>
-      
+       <div>
+      <Head>
+        <title>Whitelist Dapp</title>
+        <meta name="description" content="Whitelist-Dapp" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className={styles.main}>
+        <div>
+          <h1 className={styles.title}>Welcome to Crypto Devs!</h1>
+          <div className={styles.description}>
+            {/* Using HTML Entities for the apostrophe */}
+            It&#39;s an NFT collection for developers in Crypto.
+          </div>
+          <div className={styles.description}>
+            {numberOfWhitelisted} have already joined the Whitelist
+          </div>
+          {renderButton()}
+        </div>
+        <div>
+          <img className={styles.image} src="./crypto-devs.svg" />
+        </div>
+      </div>
+
+      <footer className={styles.footer}>
+        Made with &#10084; by Crypto Devs
+      </footer>
+    </div>
     </>
   )
 }
