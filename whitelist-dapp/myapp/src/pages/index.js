f36f4c59f3ddb8d84dd76dd3fddf49db476e0911
piyush <peeyush0409@gmail.com>
2023-03-15 23:48:44 +0530

    added two more functions

--- whitelist-dapp/myapp/src/pages/index.js
+++ whitelist-dapp/myapp/src/pages/index.js
@@ -4,7 +4,7 @@ import { providers, Contract } from 'ethers'
 import { useState, useRef, useEffect } from 'react'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-// import { WHITELIST_CONTRACT_ADDRESS, abi } from "../constants";
+import { WHITELIST_CONTRACT_ADDRESS, abi } from "../constants";
 
 
 export default function Home() {
@@ -24,15 +24,66 @@ export default function Home() {
   const web3modalRef = useRef();
 
 //we need a signer or the provider 
-const getProviderOrSigner = async (needSigner = false) => {  // param: needSigner true if the signer otherwise false
+const getProviderOrSigner = async (needSigner = false) => {  // param: needSigner true if the signer otherwise false by default
 
   const provider = await web3modalRef.current.connect();
   const web3Provider = new providers.Web3Provider(provider);
 
   // If users is not connected to the goerli network, let them know throw and error
-  const {chainID} = await web3Provider.getNetwork(provider);
+  const {chainID} = await web3Provider.getNetwork();
 
+  //what of the chain is not running on the goerli network?
+  if (chainID !== 5){
+    window.alert("Please set the network to Goerli")
+    throw new Error("Change the network to Goerli")
+  }
+
+  if(needSigner){
+    return web3Provider.getSigner();
+  }
+  return web3Provider;
+};
+
+//function for adding the current address to whitelist
+const addAddressToWhitelist = async () => {
+ try{
+  //we need a signer here since it is the write transaction
+  const signer = await getProviderOrSigner(true);
+
+  //creating the instance of the new contract with the given signer, which allows to update the method
+  const whitelistContract = new Contract(
+    WHITELIST_CONTRACT_ADDRESS,
+    abi,
+    signer
+  );
+
+  //call the whitelist address function from the contract
+  const tx = whitelistContract.addAddressToWhitelist();
+  setLoading(true);
+
+ }catch(err){
+  console.error(err);
+};
+
+//function for getting the number of the whitelisted contracts
+const getNumberOfWhitelisted = async () => {
+  try{
+    //get the provider from web3modal in case it is metamask
+    //no need of the signer here we are only reading the number of the whitelisted contracts
+    const provider = await getProviderOrSigner(); //no need to write false, its default!
+
+    const whitelistContract = new Contract(
+       WHITELIST_CONTRACT_ADDRESS,
+       abi,
+       provider
+    );
+    //calling the getNumberOfWhitelisted from the contract
+     const _numberOfWhitelistedContracts = await whitelistContract.numbgetNumberOfWhitelisted()
+  }catch(err){
+    console.error(err);
+  }
 }
+
   return (
     <>
        <div>
@@ -64,4 +115,5 @@ const getProviderOrSigner = async (needSigner = false) => {  // param: needSigne
     </div>
     </>
   )
+}
 }
